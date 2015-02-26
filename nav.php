27638d97c7319b418b535860e8a0999a10deb116
darwindiaz <darwindiaz0889@hotmail.com>
2015-02-25 22:55:50 -0500

    formulario

---
+++ nav.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es-Es">
+<head>
+	<meta charset="utf-8">
+	<title>GAMEX.COM</title>
+	<link rel="stylesheet" href="css/main.css">
+	<img src="./imag/logo.jpg" height="150" width="1360">
+</head>
+<body>
+	<form>
+		<h1>Formulario de Alquiler</h1>
+		<text>Numero de Identificacion: </text>&nbsp<input type="text" max-length="20" placeholder="Documentacion"/><br/>
+		<text>Nombres: &nbsp</text><input type="password" max-length="20" placeholder="Nombre Y Apellidos"/><br/>
+		<text>Telefono: &nbsp</text><input type="text" max-length="10" placeholder="Tel/Cel"/></br>
+		<button type="button"/>Alquilar </button><br/>
+		<a href="index.php" target="_self"> <button type="button" >Regresar</button></a>
+	</form>
+
+</body>
+</html>
