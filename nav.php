e0129ee10060586180da3d037ee3db42c9b36a39
darwindiaz <darwindiaz0889@hotmail.com>
2015-02-26 12:00:18 -0500

    pagina formulario

--- nav.php
+++ nav.php
@@ -5,15 +5,41 @@
 	<title>GAMEX.COM</title>
 	<link rel="stylesheet" href="css/main.css">
 	<img src="./imag/logo.jpg" height="150" width="1360">
+
 </head>
 <body>
-	<form>
-		<h1>Formulario de Alquiler</h1>
-		<text>Numero de Identificacion: </text>&nbsp<input type="text" max-length="20" placeholder="Documentacion"/><br/>
-		<text>Nombres: &nbsp</text><input type="password" max-length="20" placeholder="Nombre Y Apellidos"/><br/>
-		<text>Telefono: &nbsp</text><input type="text" max-length="10" placeholder="Tel/Cel"/></br>
-		<button type="button"/>Alquilar </button><br/>
-		<a href="index.php" target="_self"> <button type="button" >Regresar</button></a>
+	<?php
+	error_reporting(0);
+	$i=$_GET['imagen']-1;
+	?>
+
+	<form action="index.php" method="post">
+	<table>
+		<?php
+
+		$conexion= mysql_connect("localhost","root");
+		mysql_select_db("tiendavideojuegos",$conexion);
+
+		//consulta a base de datos
+		$consulta= mysql_query("SELECT * FROM `videojuego`");
+		$fil=mysql_num_rows($consulta);
+
+		echo '<tr><td><h2><div id=formu align="center"><input type="image" name="imagen2" value="'.mysql_result($consulta, $i,"id_v").'" src="./imag/'.mysql_result($consulta, $i,"nombre_v").'.jpg" height="400" width="1100"></div></h2>';
+		echo '<td rowspan="2"><h1 align="center">FORMATO DE ALQUILER</h1>';
+		echo '<input type="text" name="iden" id="iden" placeholder="Identificacion"/>';
+		echo '<input type="text" name="nomb" placeholder="Nombre"/>';
+		echo '<input type="text" name="tele" placeholder="Telefono"/><br>';
+		echo '<input type="submit" name ="ac" value ="ALQUILAR"/>';
+		?>
+		<?php	
+		echo '<tr><td><div id="id" align="center">'.mysql_result($consulta, $i,"nombre_v").'</div>';
+		echo '<div id="id" align="center">Descripcion: '.mysql_result($consulta, $i,"descripcion_v").'</div>';
+		echo '<div id="id" align="center">Consola: '.mysql_result($consulta, $i,"consola_v").'</div>';
+		echo '<div id="id" align="center">Stock: '.mysql_result($consulta, $i,"cantidad_v").'</div>';
+		echo '<div id="id" align="center">Valor: €€ '.mysql_result($consulta, $i,"precio_v").'</div></td></tr>';
+	
+		?>
+		</table>
 	</form>
 
 </body>
